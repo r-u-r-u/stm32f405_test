98c63f3ba7b241d83fb707f843471b8630bb58a9
WakoTakeru <t183184@edu.tut.ac.jp>
2019-06-25 01:45:29 +0900

    [add] PWMの基底クラスを追加

---
+++ Library/Timer/PWM/PWM.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "stm32f4xx_hal.h"
+
+class PWM{
+private:
+    TIM_HandleTypeDef htim;
+public:
+    PWM();
+    virtual ~PWM();
+    virtual void start(uint32_t channel);
+    virtual void stop(uint32_t channel);
+    virtual void set_channel(uint32_t channel);
+    virtual void output(uint32_t channel,float duty);
+};
